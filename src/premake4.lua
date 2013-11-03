ee6699c89d99b81882a14664fb2752934b31e6fb
jseidenz <jseidenz@gmail.com>
2013-11-03 01:19:39 -0700

    Removed the need for per project premake files.

--- src/premake4.lua
+++ src/premake4.lua
@@ -1,1 +1,32 @@
-include("apps/scml")
+if _OPTIONS.os == nil then
+   error('Please specify your target os!')
+end
+
+apps = {'scml'}
+
+for k, app in pairs(apps) do
+	solution(app)
+	   configurations { "debug", "release" }
+	   location "../gen/proj"
+
+	   configuration("windows")
+	      targetdir( "../win32/compilers" )
+	   configuration("macosx")
+	      targetdir( "../osx/compilers" )
+	   configuration("linux")
+	      targetdir( "../linux/compilers" )
+
+	   flags { "Symbols", "NoRTTI", "NoEditAndContinue", "NoExceptions", "NoPCH" }
+
+	   project(app)
+	      kind "ConsoleApp"
+	      language "C++"
+	      files { "apps/"..app.."/**.h", "apps/"..app.."/**.cpp" }      
+	 
+	      configuration "debug"
+	         defines { "DEBUG" }
+	 
+	      configuration "release"
+	         defines { "RELEASE" }
+	         flags { "Optimize" }      	
+end
