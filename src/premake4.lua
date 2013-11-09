7ed6404c197a2ba51340d90a6183e68c0546f104
jseidenz <jseidenz@gmail.com>
2013-11-09 15:12:03 -0800

    Cleaned up premake.lua.

--- src/premake4.lua
+++ src/premake4.lua
@@ -5,27 +5,26 @@ end
 apps = {'scml', 'png', 'autocompiler', 'textureconverter'}
 
 solution('mod_tools')
-for k, app in pairs(apps) do	
-	   	configurations { "debug", "release" }
-	   	location "../../ds_mod_tools_out/proj"
-	   	flags { "Symbols", "NoRTTI", "NoEditAndContinue", "NoExceptions", "NoPCH" }
-	   	includedirs { "../lib/" }
-	   	configuration { "windows" }
-	      	targetdir ( "../../ds_mod_tools_out/win32/mod_tools" )
-	   	configuration { "macosx" }
-	      	targetdir ( "../../ds_mod_tools_out/osx/mod_tools" )
-	   	configuration { "linux" }
-	      	targetdir ( "../../ds_mod_tools_out/linux/mod_tools" )
+	configurations { "debug", "release" }
+	location "../../ds_mod_tools_out/proj"
+	flags { "Symbols", "NoRTTI", "NoEditAndContinue", "NoExceptions", "NoPCH" }
+	includedirs { "../lib/" }
+	configuration { "windows" }
+	   	targetdir ( "../../ds_mod_tools_out/win32/mod_tools" )
+	configuration { "macosx" }
+	   	targetdir ( "../../ds_mod_tools_out/osx/mod_tools" )
+	configuration { "linux" }
+	  	targetdir ( "../../ds_mod_tools_out/linux/mod_tools" )
 
-       	configuration { "debug" }
-          	defines { "DEBUG" }	 
-	   	configuration { "release" }
-	        defines { "RELEASE" }
-	        flags { "Optimize" }	
+    configuration { "debug" }
+        defines { "DEBUG" }	 
+   	configuration { "release" }
+        defines { "RELEASE" }
+        flags { "Optimize" }	
 
-	   project(app)
-	      kind "ConsoleApp"
-	      language "C++"
-	      files { "app/"..app.."/**.h", "app/"..app.."/**.cpp" }      
-	 
-end
+	for k, app in pairs(apps) do	
+	   	project(app)
+			kind "ConsoleApp"
+			language "C++"   	   
+	      	files { "app/"..app.."/**.h", "app/"..app.."/**.cpp" }	 
+	end
