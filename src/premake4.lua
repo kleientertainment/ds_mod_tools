23af88d883e3b06d09b51602ce4d7fb47f832b0c
jseidenz <jseidenz@gmail.com>
2013-11-09 17:03:11 -0800

    Premake will now unzip files from the pkg folder to the output directory making it easy to change cross platform scripts.

--- src/premake4.lua
+++ src/premake4.lua
@@ -13,16 +13,19 @@ elseif os_properties[_OPTIONS.os] == nil then
 	error('Unsupported os!')
 else
 	props = os_properties[_OPTIONS.os]
+	props.outdir = "..\\..\\ds_mod_tools_out\\"
+	props.skuoutdir = props.outdir..props.dir.."\\mod_tools\\"
+	props.tooldir = '..\\tools\\bin\\'..props.dir.."\\"
 end
 
 apps = {'scml', 'png', 'autocompiler', 'textureconverter'}
 
 solution('mod_tools')
 	configurations { "debug", "release" }
-	location "../../ds_mod_tools_out/proj"
+	location ( props.outdir.."proj" )
 	flags { "Symbols", "NoRTTI", "NoEditAndContinue", "NoExceptions", "NoPCH" }
 	includedirs { "../lib/" }
-  	targetdir ( "../../ds_mod_tools_out/"..props.dir.."/mod_tools" )
+  	targetdir ( props.skuoutdir )
 
     configuration { "debug" }
         defines { "DEBUG" }	 
@@ -36,3 +39,11 @@ solution('mod_tools')
 			language "C++"   	   
 	      	files { "app/"..app.."/**.h", "app/"..app.."/**.cpp" }	 
 	end
+
+local function extract(file, folder)
+	cmd = props.tooldir..'7z.exe x '..file..' -o'..folder
+	os.execute(cmd)	
+end
+
+extract('..\\pkg\\cmn\\mod_tools.zip', props.outdir..props.dir)
+extract('..\\pkg\\'..props.dir..'\\mod_tools.zip', props.outdir..props.dir)
